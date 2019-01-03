5558b87954f3ecfc739ee6fc23a5f16afa32a588
Zac Crawford <zwcrawford@gmail.com>
2019-01-03 00:29:08 -0600

    Got the map method working. Trying filter method next

--- array-methods.js
+++ array-methods.js
@@ -10,11 +10,10 @@ planetEl.innerHTML = "<h2>List of Planets:</h2>"
 
 planets.forEach(planet => {
   // Set the content of a section with an id of planets to the following (after the heading)
-  planetEl.innerHTML += `
-    ${planet}
-  `
+  planetEl.innerHTML += `${planet} `
 });
-//****************** PART 1 - MAP ******************/
+
+//****************** PART 2 - MAP ******************/
 /*
     Use the map method to create a new array where the
     first letter of each planet is capitalized. Use the
@@ -22,8 +21,17 @@ planets.forEach(planet => {
 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 */
+const mapEl = document.getElementById("maps")
+mapEl.innerHTML = "<h2>List of Capitalized Planets:</h2>"
 
-//****************** PART 1 - FILTER ******************/
+const upperRocks = planets.map(function (planet) {
+    // Set the maps container to the Uppercase Planets
+    // Selects the first character of each planet and capitalizes it.
+    // planet.slice allows you to stop the capitalization after the first letter.
+    mapEl.innerHTML += `${planet.charAt(0).toUpperCase()}${planet.slice(1)} `
+})
+
+//****************** PART 3 - FILTER ******************/
 /*
     Use the filter method to create a new array that
     contains planets with the letter 'e'. Use the `includes()`
@@ -32,6 +40,6 @@ planets.forEach(planet => {
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 */
 
-//****************** PART 1 - REDUCE ******************/
+//****************** PART 4 - REDUCE ******************/
 // Use the reduce method to create a sentence from the words in the following array
 const words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"]
